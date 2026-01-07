2f35d6b767cee6b599c030c23a2e6bd5ce85fdf2
AbbasSavvy <abbasteen@gmail.com>
2026-01-07 13:29:46 +0530

    Adding initial game state

---
+++ rps_agent_main.py
@@ -0,0 +1,9 @@
+# Creating function for setting initial state of the game:
+def init_state():
+    init_dict = {
+        "round_number": 1,
+        "user_score": 0,
+        "bot_score": 0,
+        "user_used_bomb": False,
+        "bot_used_bomb": False
+    }
