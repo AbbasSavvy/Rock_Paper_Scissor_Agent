6a50e9a4565ebbb5001fcaf1d9d63809335e0256
AbbasSavvy <abbasteen@gmail.com>
2026-01-07 18:07:10 +0530

    Copied tools from non-api file and added tools to agent

--- rps_plus_referee/agent.py
+++ rps_plus_referee/agent.py
@@ -1,4 +1,47 @@
+import random
 from google.adk.agents.llm_agent import Agent
+
+def validate_moves(user_move, bomb_used):
+    valid_moves = {"rock", "paper", "scissors", "bomb"}
+
+    if user_move not in valid_moves:
+        return {'move_valid': False, "reason": "User move is Invalid"}
+
+    if user_move == "bomb" and bomb_used:
+        return {"move_valid": False, "reason": "Bomb already used"}
+
+    return {"move_valid": True}
+
+
+def choose_bot_move(bot_used_bomb):
+    if not bot_used_bomb:
+        moves = ["rock", "paper", "scissors", "bomb"]
+    else:
+        moves = ["rock", "paper", "scissors"]
+
+    return random.choice(moves)
+
+
+def resolve_round(user_move, bot_move):
+    if user_move == bot_move:
+        return "draw"
+
+    if user_move == "bomb":
+        return "user"
+
+    if user_move == "bomb":
+        return "user"
+
+    what_beats_what = {
+        "rock": "scissors",
+        "scissors": "paper",
+        "paper": "rock"
+    }
+
+    if bot_move == what_beats_what[user_move]:
+        return "user"
+    else:
+        return "bot"
 
 root_agent = Agent(
     name="rps_plus_referee",
@@ -21,5 +64,10 @@ root_agent = Agent(
         '  "winner": "user" | "bot" | "draw",\n'
         '  "message": string\n'
         "}"
-    )
+    ),
+    tools=[
+        validate_moves,
+        choose_bot_move,
+        resolve_round,
+    ],
 )
